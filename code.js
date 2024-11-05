76806fd86f0ddbccf8fc8192bf8aa0159a6f0bf4
S A <sa@Ss-MacBook-Air.local>
2024-11-05 16:37:33 -0500

    First Commit

---
+++ code.js
@@ -0,0 +1,18 @@
+/****
+ * 1) Declare a string variable and assign it to the value 
+ * "You have received this message because you have been chosen to open an important vault "
+ 2) Declar and assign 3 variables, one for each number
+3) Code1 will be using addition 
+4) Code2 will be using substraction 
+5) Code3 will be using multiplication 
+6) Display the string and the codes to the user
+*/
+
+
+const userMessage = "You have recieved this message because you have been chosen to open an important vault"
+
+let code1 = 1 + 1
+let code2 = 10 - 5
+let code3 = 7 * 3
+
+alert(userMessage + "\n" + code1 + " " + code2 + " " + code3)
